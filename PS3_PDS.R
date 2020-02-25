1584a684b086ca23f0d1814a1e7cb5b7b6ea205b
Jin <60522038+dahjinkim@users.noreply.github.com>
2020-02-25 07:59:06 -0600

    started #2, 3

--- PS3_PDS.R
+++ PS3_PDS.R
@@ -15,8 +15,8 @@ primaryPolls <- read.csv('https://jmontgomery.github.io/PDS/Datasets/president_p
 primaryPolls$start_date <- as.Date(primaryPolls$start_date, "%m/%d/%Y")
 
 #subset by relevant states and candidates
-march3States <- c("Alabama", "Arkansas", "California", "Colorado", "Maine", "Massachusetts", "Minnesota", "North Carolina", "Oklahoma", "Tennessee", "Texas", "Utah", "Vermont", "Virginia")
-primaryPolls <- primaryPolls[primaryPolls$state %in% March3states, ]
+March3States <- c("Alabama", "Arkansas", "California", "Colorado", "Maine", "Massachusetts", "Minnesota", "North Carolina", "Oklahoma", "Tennessee", "Texas", "Utah", "Vermont", "Virginia")
+primaryPolls <- primaryPolls[primaryPolls$state %in% March3States, ]
 relevantCandidates <- c("Amy Klobuchar", "Bernard Sanders", "Elizabeth Warren", "Joseph R. Biden Jr.", "Michael Bloomberg", "Pete Buttigieg", "Tom Steyer")
 primaryPolls <- primaryPolls[primaryPolls$candidate_name%in% relevantCandidates, ]
 
@@ -26,6 +26,7 @@ graph1 <- ggplot(data=primaryPolls)+
   geom_smooth(mapping = aes(x=start_date, y=pct, color=candidate_name)) + 
   geom_point(mapping = aes(x=start_date, y=pct, color=candidate_name), alpha=.4) +
   facet_wrap(~ candidate_name, nrow=2)
+graph1
 
 #plotting in minimal theme
 graph1 <- graph1 + theme_minimal()
@@ -35,10 +36,40 @@ graph1
 graph1 <- graph1 + labs(x = "Dates", y = "Poll Percentage") + ylim(-10, 50) 
 graph1
 
-#plotting with customized legend name ("Candidates")
+#plotting with customized legend name ("Relevant Candidates")
 graph1 <- graph1 + scale_colour_discrete(name="Relevant\nCandidates")
 graph1
 
 
 
 ##### 2
+
+#subset the data with candidates who had at least 5 polls in one state
+test <- primaryPolls %>%
+  select(state, candidate_name, pct) %>%
+  group_by(candidate_name, state) %>%
+  mutate(count = n()) %>%
+  filter(count >= 5) %>%
+  arrange(state, candidate_name, desc(pct))
+candidate.names <- levels(as.factor(test$candidate_name))
+
+#subset the data by candidates using a for loop
+#create a null list
+candidate_list <- NULL
+for (i in candidate.names){
+  subset.i <- test[test$candidate_name==i, ]
+  candidate_list[[i]] <- subset.i
+}
+
+Klobuchar <- candidate_list[[1]]
+
+
+head(candidate_list)
+
+
+##### 3
+library(fivethirtyeight)
+library(tidyverse)
+polls <- read_csv('https://jmontgomery.github.io/PDS/Datasets/president
+_primary_polls_feb2020.csv')
+Endorsements <- endorsements_2020
